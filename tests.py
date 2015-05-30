a7bab3f8ed9c095d376b933e7d1dac79a2862389
David Townshend <david.townshend@aurecongroup.com>
2015-05-30 19:35:15 +0200

    Fix failing tests

--- tests.py
+++ tests.py
@@ -4,16 +4,23 @@ from unittest.mock import Mock, patch
 import tempfile
 import os.path
 
-import pyle
+from pyle import Pyle
+
+
+class TestCase(TestCase):
+
+    def setUp(self):
+        self.mself = Mock()
 
 
 class TestFindConfig(TestCase):
 
     def test_local_config_file(self):
+        self.mself.args.search_parents = False
         with tempfile.TemporaryDirectory() as tmp:
-            with open(os.path.join(tmp, 'venvrc.py'), 'w') as fh:
+            with open(os.path.join(tmp, 'venvrc.json'), 'w') as fh:
                 fh.write('pass')
-            result = pyle.find_config(tmp, False)
+            result = Pyle.find_config(self.mself, tmp)
             self.assertEqual(result, tmp)
 
 
@@ -21,14 +28,14 @@ class TestParseArgv(TestCase):
 
     def test_all_long_args(self):
         argv = ['--create', '--search-parents', 'python']
-        args = pyle.parse_argv(argv)
+        args = Pyle.parse_argv(self.mself, argv)
         self.assertTrue(args.create)
         self.assertTrue(args.search_parents)
         self.assertEqual(args.args, ['python'])
 
     def test_all_short_args(self):
         argv = ['-c', '-p', 'python']
-        args = pyle.parse_argv(argv)
+        args = Pyle.parse_argv(self.mself, argv)
         self.assertTrue(args.create)
         self.assertTrue(args.search_parents)
         self.assertEqual(args.args, ['python'])
@@ -39,6 +46,7 @@ class TestEnsureVenv(TestCase):
     def test_venv_exists(self):
         with tempfile.TemporaryDirectory() as tmp:
             venv_path = os.path.join(tmp, '.venv')
+            self.mself.venv_path = venv_path
             os.mkdir(venv_path)
-            result = pyle.ensure_venv(tmp, {'VENV_ROOT': '.venv'})
-            self.assertEqual(result, venv_path)
+            # Should run withing error
+            Pyle.ensure_venv(self.mself)
